54fb0f5f54cbfca7de23fec2b7874249b65d4161
Sebastian Rojas Gonzalez <rojasgonzales0714@gmail.com>
2025-06-12 11:19:50 -0500

    Migraciones
    Se crean las migraciones de la base de datos persa

--- persa/database/migrations/2025_06_12_150528_create_users_table.php
+++ persa/database/migrations/2025_06_12_150528_create_users_table.php
@@ -11,12 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+       Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('fullname', 50)->comment('Nombre completo');
+            $table->string('email', 50)->unique()->comment('Correo electrónico');
+            $table->string('password', 50)->comment('Contraseña');
+            $table->string('status', 50)->comment('Estado');
+            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade')->onUpdate('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ persa/database/migrations/2025_06_12_152749_create_instructor_course_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('instructor_course', function (Blueprint $table) {
+            $table->foreignId('instructor_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('course_id')->constrained('course')->onDelete('cascade')->onUpdate('cascade');
+            $table->primary(['course_id', 'instructor_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('instructor_course');
+    }
+};
