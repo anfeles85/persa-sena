2124099f4fdf690dbae18c5d518b301c4a86c162
Sebastian Rodriguez Cruz <sr1290853@gmail.com>
2025-06-24 09:07:30 -0500

    Index PERSA
    Se creo la vista index de persa y se modificaron las rutas

---
+++ persa/resources/views/index.blade.php
@@ -0,0 +1,23 @@
+@extends('templates.base')
+@section('title', 'Inicio')
+@section('header', 'Inicio')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 mb-4">
+            <p align = "justify">
+                PERSA es un aplicativo para la administración de permisos y salidas de los aprendices.
+                En él podra gestionar los siguientes módulos:
+            </p>
+            <p>
+                <ul>
+                    <li>Usuarios</li>
+                    <li>Permisos</li>
+                    <li>Tipo de permisos</li>
+                    <li>Fichas</li>
+                    <li>Sedes</li> 
+                </ul>
+            </p>
+        </div>
+    </div>
+    
+@endsection
