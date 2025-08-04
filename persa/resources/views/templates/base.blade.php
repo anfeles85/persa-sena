f1e82023d4e87c4c0120af20af632d5497f7aa7f
Juan Sebastian Rodriguez Cruz <sr1290853@gmail.com>
2025-08-04 13:04:45 -0500

    Correcion visual
    Se corregieron errores visuales

--- persa/resources/views/templates/base.blade.php
+++ persa/resources/views/templates/base.blade.php
@@ -1,21 +1,103 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Iniciar Sesión - P.E.R.S.A</title>
+<!DOCTYPE html>
+<html lang="en">
 
-    <!-- Fonts and icons -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-    <!-- Nucleo Icons -->
-    <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-icons.css" rel="stylesheet" />
-    <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-svg.css" rel="stylesheet" />
-    <!-- CSS Files -->
-    <link id="pagestyle" href="{{ asset('css/argon-dashboard.css?v=2.1.0') }}" rel="stylesheet" />
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-    <!-- DataTables CSS -->
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" type="image/png" href="{{ asset('img/sena-logo.png') }}">
+  <title>
+    @yield('title')
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-svg.css" rel="stylesheet" />  
+  <!-- CSS Files -->
+  <link id="pagestyle" href="{{ asset('css/argon-dashboard.css?v=2.1.0') }}" rel="stylesheet" />
+  <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+  <!-- DataTables CSS -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
     <!-- DataTables JS -->
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+
+
 </head>
+
+<body class="g-sidenav-show   bg-gray-100">
+    <div class="min-height-90 position-absolute w-100" style="background-color: #00304D;"></div>
+    @include('templates.nav')
+
+    <main class="main-content position-relative border-radius-lg ">
+        @include('templates.topbar')
+
+        <div class="container-fluid py-4">
+            <div class="row">
+                <div class="col-12">
+                    @yield('content')
+                </div>  
+            </div>     
+        </div>
+
+        @include('templates.footer')
+
+    </main>
+
+    <script src="{{ asset('js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/smooth-scrollbar.min.js') }}"></script>
+    <script>
+        var win = navigator.platform.indexOf('Win') > -1;
+        if (win && document.querySelector('#sidenav-scrollbar')) {
+        var options = {
+            damping: '0.5'
+        }
+        Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+        }
+        
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/all.min.js"></script>
+
+    <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="{{ asset('js/argon-dashboard.min.js?v=2.1.0')}}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="{{ asset('js/general.js') }}"></script>
+    
+    @if(session('success'))
+    <script>
+        Swal.fire({
+            icon: 'success',
+            title: '¡OK!',
+            text: '{{ session("success") }}',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'OK'
+        });
+    </script>
+    @endif
+
+
+    @if(session('error'))
+    <script>
+        Swal.fire({
+            icon: 'error',
+            title: '¡Error!',
+            text: '{{ session("error") }}',
+            confirmButtonColor: '#d33',
+            confirmButtonText: 'OK'
+        });
+    </script>
+    @endif
+
+    @if(session('updated'))
+    <script>
+        update(); 
+    </script>
+    @endif
+
+    @yield('scripts')
+</body>
+</html>    
