d4af8c73b77d6ae9475ce4f77f7cf82b1622e5ed
JUAN FERNANDO VELASQUEZ <juanchibiris80@gmail.com>
2025-08-06 09:05:20 -0500

    Se ajusto user
    En USER se añadió el documento

--- persa/app/Models/User.php
+++ persa/app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'fullname',
         'email',
+        'document',
         'password',
         'status',
         'role_id'
