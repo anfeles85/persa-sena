e8a361063d30d309710518f3ed7c9107aa1a5ec7
Tatiana Carvajal <carvajaltatiana662@gmail.com>
2025-08-19 14:18:19 -0500

    Kernel
    se agrego la hora de ejecutar el comando al kernel

--- persa/app/Console/Kernel.php
+++ persa/app/Console/Kernel.php
@@ -12,7 +12,12 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+          $schedule->command('status:cancel-permissions')
+                 ->dailyAt('06:00')
+                 ->timezone('America/Bogota') 
+                 ->withoutOverlapping() 
+                 ->onOneServer() 
+                 ->runInBackground();
     }
 
     /**
@@ -23,5 +28,8 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+  
+  
+  
     }
 }
