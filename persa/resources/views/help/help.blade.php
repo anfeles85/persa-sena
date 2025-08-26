7f49d679d14de8941c4635daf4f0fabfe8a7660c
Tatiana Carvajal <carvajaltatiana662@gmail.com>
2025-08-26 08:40:57 -0500

    Help
    se creo la pagina de help

---
+++ persa/resources/views/help/help.blade.php
@@ -0,0 +1,13 @@
+@extends('templates.base')
+@section('title', 'Ayuda')
+@section('header', 'Ayuda')
+
+@push('styles')
+    <!-- Fonts and icons -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link id="pagestyle" href="../assets/css/material-dashboard.css?v=3.2.0" rel="stylesheet" />
+@endpush
