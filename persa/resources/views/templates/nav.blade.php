57455f09e5f84b73b38d018049cc30ed9150081c
Juan Sebastian Rodriguez Cruz <sr1290853@gmail.com>
2025-08-30 20:01:20 -0500

    Aside
    Se reajustó el asidebar para una mejor navegacion

--- persa/resources/views/templates/nav.blade.php
+++ persa/resources/views/templates/nav.blade.php
@@ -1,17 +1,19 @@
-<aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
+<aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3" id="sidenav-main">
   <div class="sidenav-header">
-    <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+    <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
+       aria-hidden="true" id="iconSidenav"></i>
     <a class="navbar-brand m-3">
       <img src="{{ asset('img/persa-logo.png') }}" class="img-fluid navbar-brand-img" style="max-height: 100px;" alt="main_logo">
     </a>
   </div>
   <hr class="horizontal dark mt-0">
-  <div class="collapse navbar-collapse w-auto " id="sidenav-collapse-main">
+  <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
     <ul class="navbar-nav">
 
       @can('coordinador')
       <li class="nav-item">
-        <a class="nav-link {{ request()->routeIs('user.*') ? 'active' : '' }}" href="{{ route('user.index') }}">
+        <a class="nav-link {{ (request()->routeIs('user.*') && !request()->routeIs('user.profile')) ? 'active' : '' }}"
+           href="{{ route('user.index') }}">
           <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
             <i class="fas fa-user text-dark text-sm opacity-10"></i>
           </div>
@@ -101,7 +103,7 @@
 
       @canany(['coordinador-instructor','aprendiz'])
       <li class="nav-item">
-        <a class="nav-link {{ request()->routeIs('auth*') ? 'active' : '' }}" href="{{ route('user.profile') }}">
+        <a class="nav-link {{ request()->routeIs('user.profile') ? 'active' : '' }}" href="{{ route('user.profile') }}">
           <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
             <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
           </div>
@@ -119,18 +121,16 @@
         </a>
       </li>
 
-       <!-- Separador para Ayuda -->
       <hr class="horizontal dark mt-4 mb-2">
-      
-      <!-- Sección de Ayuda separada -->
+
       <li class="nav-item mt-3">
         <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Soporte</h6>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="{{ route('help.help') }}" target="_blank">
+        <a class="nav-link" href="{{ route('help.help') }}">
           <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="fa-solid fa-circle-question text-dark text-sm opacity-10"></i>
+            <i class="fa-solid fa-circle-question text-dark text-sm opacity-10"></i>
           </div>
           <span class="nav-link-text ms-1">Ayuda</span>
         </a>
