102ed281ecaa2e3cee4c6939b813383c7e4d6e6b
Juan Sebastian Rodriguez Cruz <sr1290853@gmail.com>
2025-10-22 18:52:44 -0500

    Auth
    (STYLE) Se agregó un favicon o logo en pestaña con el logo SENA para una mejor visualización

--- persa/resources/views/auth/login.blade.php
+++ persa/resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="{{ asset('img/sena-logo.png') }}">
     <title>Login</title>
 
     <!-- Custom fonts for this template-->
